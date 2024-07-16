6b1bd4308159efeec10e370fdfad1a03957501ae
lasweswqsq <2764783923@qq.com>
2024-07-16 10:51:19 +0800

    00000

---
+++ list/exercise3.8~3.10.py
@@ -0,0 +1,52 @@
+w=['Peking','Hawaii','Tokyo','Kamiyosato ayaka','Michigan']
+print(w)
+print(sorted(w))
+print(w)
+print(sorted(w,reverse=True))
+print(w)
+w.reverse()
+print(w)
+w.reverse()
+print(w)
+w.sort()
+print(w)
+w.sort(reverse=True)
+print(w)
+print(len(w))
+
+
+#print(w[-46465])
+
+#Traceback (most recent call last):
+#  File "c:\Users\Sunny\doctor\list\exercise3.8~3.10.py", line 16, in <module>
+#    print(w[-46465])
+#          ~^^^^^^^^
+#IndexError: list index out of range
+
+
+#SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 46-47: truncated \UXXXXXXXX escape
+#PS C:\Users\Sunny\doctor> & C:/Users/Sunny/AppData/Local/Programs/Python/Python312/python.exe c:/Users/Sunny/doctor/list/exercise3.8~3.10.py
+#  File "c:\Users\Sunny\doctor\list\exercise3.8~3.10.py", line 17
+#    '''
+#    ^^^
+#SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 46-47: truncated \UXXXXXXXX escape
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
