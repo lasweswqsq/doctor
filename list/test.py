1ef6ac3e7edd1c79bb4b3e94651509a766273dfc
lasweswqsq <2764783923@qq.com>
2024-07-16 10:21:32 +0800

    abaajiang

---
+++ list/test.py
@@ -0,0 +1,10 @@
+#TypeError: '<' not supported between instances of 'str' and 'int'
+#what?!
+cret=[24354,3465,2123,490,888,43]
+cret.sort()
+print(cret)
+
+
+cret=['2','shgaf','3.455','GFYDFT','"']
+cret.sort()
+print(cret)
