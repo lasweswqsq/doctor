5eab92791baf12638719241871cde9e5f4eafc12
lasweswqsq <2764783923@qq.com>
2024-07-16 15:49:39 +0800

    jeans

---
+++ if/move your hands5.8~5.11.py
@@ -0,0 +1,31 @@
+reader=['one is available','defend board beautiful piano','white well black chid','hinata','admin','administratoe','up stripe when string']
+for i in reader:
+    if (i == 'admin'):
+        print('you are the god, admin. what can i do for you\n')
+    else:
+        print('you idiot',i,"next time i won't let you in.\n")
+
+challengers=[]
+if challengers == []:
+    print ("why? you monsters!")
+
+passport=['nahahaha','one is available','goblin superb','defend board beautiful piano','white well black chid','lotus','butcher','prince']
+for i in passport:
+    if i not in reader:
+        print('you successfully setup a user name')
+        reader.append(i)
+    elif i in reader:
+        print('badgate@')
+
+    print('now we have:',reader,'\n')
+
+
+
+
+
+
+
+
+
+
+
