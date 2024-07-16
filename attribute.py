1d536518ee8566fbc04f435a1ef316a97ce9f188
lasweswqsq <2764783923@qq.com>
2024-07-16 08:33:37 +0800

    3bb

--- attribute.py
+++ attribute.py
@@ -2,3 +2,13 @@ name="wis' adel"
 print(name.title())
 print(name.upper())
 print(name.lower())
+
+f='ejafarra'
+l='the hvit aska'
+full=f+' '+l
+print(full.title())
+print(full.upper())
+full=f'{f}{' '}{l}'
+print(full)
+
+print('nearl\tthe\n\tradiant\tknight')
