0e997be52c8b8fb9f46a8d481757dac9a4c94041
lasweswqsq <2764783923@qq.com>
2024-07-18 16:11:07 +0800

    baseball

--- class/exrecise 9.4~9.5.py
+++ class/exrecise 9.4~9.5.py
@@ -22,3 +22,33 @@ s=user(250,27)
 user.sqrt(s)
 user.average(s)
 user.variance(s)
+
+class upper(user):
+    def __init__(self, y, z):
+        self.y=y
+        self.z=z
+        #initialize father category
+        super().__init__(y, z)
+    
+    def nmb(self):
+        print(self.y)
+        while(self.y<=2020):
+            self.y=self.y+1
+            q=1
+            cnt=0
+            while(q<self.y-1):
+                q=q+1
+                if (self.y%q==0):
+                    cnt+=1
+                
+            if (cnt==0):
+                print(self.y,'is a prime number')
+            else:
+                print(self.y,'is a composite number, it has',cnt+2,'prime factors.')
+        
+
+p=upper(2,2132)
+upper.nmb(p)
+upper.average(p)
+
+
