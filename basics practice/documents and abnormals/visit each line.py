3aaf1bbcbe7c14b295128e42eb3ce61dd24d7af5
lasweswqsq <2764783923@qq.com>
2024-07-19 15:38:12 +0800

    portu

--- basics practice/documents and abnormals/visit each line.py
+++ basics practice/documents and abnormals/visit each line.py
@@ -4,19 +4,23 @@ import pprint
 path=Path("blog.txt")
 c=path.read_text()
 l=c.splitlines()
-'''
+
 pprint.pprint(l)
 for i in l:
     print(i)
-'''
+
 
 d=''
 for i in l:
-    d += i
 
+    d += f"i like {i}"
+
+
+#d.replace('i','you  ')
 print(d,'\n',len(d))
 
-
+p=Path('666.txt')
+p.write_text(d)
 
 
 
