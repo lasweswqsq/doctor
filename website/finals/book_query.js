a827c594f34d315e069476ae6fe7df96a29e0415
lasweswqsq <2764783923@qq.com>
2024-08-08 10:20:54 +0800

    love

--- website/finals/book_query.js
+++ website/finals/book_query.js
@@ -4,3 +4,13 @@ $(function(){
         
     })
 })
+
+
+fetch('/query_db?query=SELECT%20*%20FROM%20book')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // 处理数据库返回的结果
+  })
+  .catch(error => console.error('Error:', error));
+
